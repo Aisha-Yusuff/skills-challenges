73c54d531c5a77e697201631776538f18e286a33
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-05-23 14:40:31 +0100

    change file name

---
+++ Intermezzo1/lib/common_letter.rb
@@ -0,0 +1,18 @@
+def get_most_common_letter(text)
+  counter = Hash.new(0)
+  text.chars.each do |char|
+    if !char.include?(" ")
+    counter[char] += 1
+    end
+  end
+  last_index = counter.to_a.length - 1
+  counter.to_a.sort_by { |k, v| v }[last_index][0]
+end
+
+
+p get_most_common_letter("the roof, the roof, the roof is on fire!")
+
+# Intended output:
+# 
+# > get_most_common_letter("the roof, the roof, the roof is on fire!")
+# => "o"
--- lib/reading_time.rb
+++
@@ -1,5 +0,0 @@
-def calculate_time(text)
-  words = text.split(" ")
-  return (words.length / 200.to_f) .ceil
-end 
-
